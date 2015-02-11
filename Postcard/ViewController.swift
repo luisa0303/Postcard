fb163f21ef6a707e3d2f0f13c1db3fafae7d0319
Luisa <macbookpro@MacBook-Pro-de-MacBook-2.local>
2015-02-11 19:36:23 -0200

    third rev or project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
@@ -42,6 +43,13 @@ class ViewController: UIViewController {
     
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        
     }
 
     
